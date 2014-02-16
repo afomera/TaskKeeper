49bc28990ecbc31b1add128c1b1d437d871fd755
unknown <kaptoye@gmail.com>
2014-02-16 02:37:31 -0600

    Initial Repo Setup, using bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaskKeeper::Application.config.secret_key_base = '3a156bb826bea18567d73e75755d5d08babf0fab2def4757e8b5b65a14c3f273011ae374a70a70d0e4441394f8cfe22b1fe2c16cc32a5bb5bc7d95074d70d130'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaskKeeper::Application.config.session_store :cookie_store, key: '_TaskKeeper_session'
